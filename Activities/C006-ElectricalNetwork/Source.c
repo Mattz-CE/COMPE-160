c2dbeb2a2da9f8f6697e7a0c7b102d78615d9551
Matt <mattz0516@outlook.com>
2020-03-06 07:41:00 +0900

    More updates

--- Activities/C006-ElectricalNetwork/Source.c
+++ Activities/C006-ElectricalNetwork/Source.c
@@ -19,7 +19,7 @@
 **    Algorithm:  Using Dot product, the value of sets of voltage input will output as current sources f.
 **
 **      Caveats:
-**      Version:  1.0.0
+**      Version:  1.0.1
 **
 */
 
@@ -38,14 +38,14 @@ int a[M][N] =
 	 {0, -1, 0, 1},
 	 {0, 0, -1, 1}};
 int b[M] = {0, 12, 0, 0, 0, 0};
-int u[N] = {-1, 1, 0, 0};
-
+int u[N] = {1, -1, 0, 0};
 int e[M] = {2, 1, -1, 1, -1, 0};
 double c[M][M] = {0};
 double w[M] = {0};
 // double w[M] = {.2, 1, -1, 1, -1, 0};
 double at[N][M];
 double f[4] = {-2.2, 2.2, 0, 0};
+
 int dot_product(int v[], int u[], int n)
 {
 	int result = 0;
@@ -74,37 +74,34 @@ void amnout()
 void emout()
 {
 	printf("Voltage Drops e[6]:\n");
-	int v = 0;
-	for (int i; i < M; i++)
+	for (int i = 0; i < M; i++)
 	{
-		for (int j; j < N; j++)
+		double dot = 0.0;
+		for (int j = 0; j < N; j++)
 		{
-			if (v < M)
-			{
-				e[v] += a[i][j] * u[j];
-			}
+			dot += a[i][j] * u[j];
 		}
-		v++;
+		e[i] = b[i] - dot;
 	}
 
 	for (int i = 0; i < 6; i++)
 	{
 		printf("%d\n", e[i]);
-	}printf("\n");
+	}
+	printf("\n");
 }
-void dotproduct();
 void cmmout()
 {
 	printf("C[M][M] Array:\n");
-	for (int i = 1; i < 5; i++)
+	for (int i = 1; i < 6; i++)
 	{
 		c[i][i] = 1;
-	}
+	}//Initializatio for CMM array
 	c[0][0] = 0.1;
 	for (int i = 1; i < 6; i++)
 	{
 		c[i][i] = 1;
-	}
+	}//Initializatio for CMM array
 
 	for (int i = 0; i < 6; i++)
 	{
@@ -122,17 +119,14 @@ void cmmout()
 void wout()
 {
 	printf("Current w[M]:\n");
-	int v = 0;
-	for (int i; i < M; i++)
+	for (int i = 0; i < M; i++)
 	{
-		for (int j; j < M; j++)
+		double dot = 0.0;
+		for (int j = 0; j < M; j++)
 		{
-			if (v < M)
-			{
-				w[v] += c[i][j] * e[j];
-			}
+			dot += c[i][j] * e[j];
 		}
-		v++;
+		w[i] = dot;
 	} //w[i] == dotproduct(c[i], w[i]);
 
 	for (int i = 0; i < 6; i++)
@@ -152,7 +146,6 @@ void reverseA()
 			at[i][j] = a[j][i];
 			printf("%0.1lf    ", at[i][j]);
 		}
-
 		printf("\n");
 		printf("\n");
 	}
@@ -163,18 +156,29 @@ void reverseA()
 void functionf()
 {
 	printf("Current source f[N]:\n");
-	int v = 0;
-	for (int i; i < N; i++)
+	// int v = 0;
+	// for (int i; i < N; i++)
+	// {
+	// 	for (int j; j < M; j++)
+	// 	{
+	// 		if (v < M)
+	// 		{
+	// 			f[v] += at[i][j] * w[j];
+	// 		}
+	// 	}
+	// 	v++;
+	// }
+
+for (int i = 0; i < N; i++)
 	{
-		for (int j; j < M; j++)
+		double dot = 0.0;
+		for (int j = 0; j < M; j++)
 		{
-			if (v < M)
-			{
-				f[v] += at[i][j] * w[j];
-			}
+			dot += at[i][j] * w[j];
 		}
-		v++;
+		f[i] = dot;
 	}
+
 	//w[i] == dotproduct(at[i], w[i]);
 	for (int i = 0; i < 4; i++)
 	{
