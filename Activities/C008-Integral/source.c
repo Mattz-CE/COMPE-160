39b803ee6afa3ce388f7812428b9dd786527ba83
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-04-07 16:51:39 -0700

    Update source.c

--- Activities/C008-Integral/source.c
+++ Activities/C008-Integral/source.c
@@ -30,89 +30,55 @@
 #include <string.h>
 #include <time.h>
 #include <string.h>
-#define N 10000
-#define Cap 100
 
-int a[N] = {0};
-int b[N] = {0};
-int sc1, sc2, sc3 = 0;
+#define bottom 1
+#define upper 2
 
-void swap(int *x, int *y)
+double a = bottom, b = upper, y , x = 1;
+
+double f(double x)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    return 1 / x;
 }
 
-void sort()
+double rectangle_method(double a, double b, int N)
 {
-    for (int pass = 1; pass < N; ++pass)
-        for (int i = 0; i < N - pass; ++i)
-            if (a[i] > a[i + 1])
-            {
-                swap(&a[i], &a[i + 1]);
-                sc1++;
-            }
-    for (int i = 0; i < N; i++)
-        printf("%4d", a[i]);
-    puts("");
-    printf("\n");
-}
-
-void selectionSort()
-{
-    int i, j, min;
-    for (i = 0; i < N - 1; i++)
+    double dx = 0, temp = 0, area = 0;
+    dx = (b - a) / N;
+    for(int i = 0; i < N; i++)
     {
-        min = i;
-        for (j = i + 1; j < N; j++)
-            if (b[j] < b[min])
-            {
-                min = j;
-                sc2++;
-            }
-        swap(&b[min], &b[i]);
+        temp = (a + dx / 2) + i * dx;
+        // arr[i] = (i / N+ (i+1) / N); 
+        area = area + f(temp) * dx;
     }
-    for (int i = 0; i < N; i++)
-        printf("%4d", b[i]);
+    return area;
 }
 
-int binarySearch(int a[], int l, int r, int z) 
-{ 
-    sc3++;
-    if (r >= l) { 
-        int mid = l + (r - l) / 2; 
-        if (a[mid] == z) 
-            return mid; 
-        if (a[mid] > z) 
-            return binarySearch(a, l, mid - 1, z); 
-        return binarySearch(a, mid + 1, r, z); 
-    }
-    return -1; 
-} 
-
+double trapezoid_method(double a, double b, int N)
+{
+    
+    return 0;
+}
 
 int main()
 {
-    srand(time(0));
-    for (int i = 0; i < N; i++)
-    {
-        a[i] = 1 + rand() % Cap;
-        b[i] = a[i];
-    }
-    sort();
-    selectionSort();
-    printf("\nSwap 1 called: %d\n", sc1);
-    printf("Swap 2 called: %d\n", sc2);
+    double result = rectangle_method(1,2,10);
+    printf("The result for 10 interval rectangle method integration will be: %f\n" , result);
+    result = rectangle_method(1,2,100);
+    printf("The result for 100 interval rectangle method integration will be: %f\n" , result);
+    result = rectangle_method(1,2,1000);
+    printf("The result for 1000 interval rectangle method integration will be: %f\n" , result);
+    result = rectangle_method(1,2,10000);
+    printf("The result for 10000 interval rectangle method integration will be: %f\n" , result);
 
-    int n = sizeof(a) / (2 * sizeof(a[0]));
-    int j = rand() % Cap;
-    printf("\nj = %d\n" , j); 
-    int z = a[j];
-    printf("\nz = %d\n" , z); 
-    int result = binarySearch(a, 0, n - 1, z); 
-    (result == -1) ? printf("Element is not present in array") 
-                   : printf("Element is present at index %d", result); 
-    printf("\nSearch called: %d\n", sc3);
+    result = trapezoid_method(1,2,10);
+    printf("The result for 10 interval trapezoid method integration will be: %f\n" , result);
+    result = trapezoid_method(1,2,100);
+    printf("The result for 100 interval trapezoid method integration will be: %f\n" , result);
+    result = trapezoid_method(1,2,1000);
+    printf("The result for 1000 interval trapezoid method integration will be: %f\n" , result);
+    result = trapezoid_method(1,2,10000);
+    printf("The result for 10000 interval trapezoid method integration will be: %f\n" , result);
+
     return 0;
 }
