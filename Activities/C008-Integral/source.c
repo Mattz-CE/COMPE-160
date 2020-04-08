8102a79103e6fc332dbefabae7ae71fb8c920f8f
Matt <33806410+Matthewzhou809@users.noreply.github.com>
2020-04-07 17:52:49 -0700

    Uploads archive

--- Activities/C008-Integral/source.c
+++ Activities/C008-Integral/source.c
@@ -34,7 +34,7 @@
 #define bottom 1
 #define upper 2
 
-double a = bottom, b = upper, y , x = 1;
+double a = bottom, b = upper, y, x = 1;
 
 double f(double x)
 {
@@ -45,10 +45,9 @@ double rectangle_method(double a, double b, int N)
 {
     double dx = 0, temp = 0, area = 0;
     dx = (b - a) / N;
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         temp = (a + dx / 2) + i * dx;
-        // arr[i] = (i / N+ (i+1) / N); 
         area = area + f(temp) * dx;
     }
     return area;
@@ -56,29 +55,43 @@ double rectangle_method(double a, double b, int N)
 
 double trapezoid_method(double a, double b, int N)
 {
-    
-    return 0;
+    double rec = 0, tri = 0, dx = 0, temp = 0, area = 0;
+    dx = (b - a) / N;
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (a + dx * i < a + dx * (i + 1))
+        {
+            rec = f(a + dx * i) * dx;
+        }
+        else
+        {
+            rec = f(a + dx * (i + 1)) * dx;
+        }
+        tri = abs(f(a + dx * (i + 1)) - f(a + dx * i) * dx / 2);
+        area = area + rec + tri;
+    }
+    return area;
 }
 
 int main()
 {
-    double result = rectangle_method(1,2,10);
-    printf("The result for 10 interval rectangle method integration will be: %f\n" , result);
-    result = rectangle_method(1,2,100);
-    printf("The result for 100 interval rectangle method integration will be: %f\n" , result);
-    result = rectangle_method(1,2,1000);
-    printf("The result for 1000 interval rectangle method integration will be: %f\n" , result);
-    result = rectangle_method(1,2,10000);
-    printf("The result for 10000 interval rectangle method integration will be: %f\n" , result);
+    double result = rectangle_method(1, 2, 10);
+    printf("\nThe result for 10 interval rectangle method integration will be: %f\n", result);
+    result = rectangle_method(1, 2, 100);
+    printf("The result for 100 interval rectangle method integration will be: %f\n", result);
+    result = rectangle_method(1, 2, 1000);
+    printf("The result for 1000 interval rectangle method integration will be: %f\n", result);
+    result = rectangle_method(1, 2, 10000);
+    printf("The result for 10000 interval rectangle method integration will be: %f\n", result);
 
-    result = trapezoid_method(1,2,10);
-    printf("The result for 10 interval trapezoid method integration will be: %f\n" , result);
-    result = trapezoid_method(1,2,100);
-    printf("The result for 100 interval trapezoid method integration will be: %f\n" , result);
-    result = trapezoid_method(1,2,1000);
-    printf("The result for 1000 interval trapezoid method integration will be: %f\n" , result);
-    result = trapezoid_method(1,2,10000);
-    printf("The result for 10000 interval trapezoid method integration will be: %f\n" , result);
+    result = trapezoid_method(1, 2, 10);
+    printf("\nThe result for 10 interval trapezoid method integration will be: %f\n", result);
+    result = trapezoid_method(1, 2, 100);
+    printf("The result for 100 interval trapezoid method integration will be: %f\n", result);
+    result = trapezoid_method(1, 2, 1000);
+    printf("The result for 1000 interval trapezoid method integration will be: %f\n", result);
+    result = trapezoid_method(1, 2, 10000);
+    printf("The result for 10000 interval trapezoid method integration will be: %f\n", result);
 
     return 0;
 }
